ae97b192ede9d77bd10aafbb7a300d80bd4c55eb
developer-kp <arunkumarp.in@hotmail.com>
2021-05-18 00:14:02 +0200

    adding specs for no duplicates and multiple duplicates

--- src/find-duplicates.spec.ts
+++ src/find-duplicates.spec.ts
@@ -1,57 +1,168 @@
 import { findDuplicateTransactions, Transaction } from './find-duplicates';
 
 test('Find duplicate transactions', () => {
-  const transactions: Transaction[] = [
-    {
-      id: 1,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
-    },
-    {
-      id: 2,
-      source: 'B',
-      target: 'C',
-      amount: 1000,
-      description: 'rent'
-    },
-    {
-      id: 3,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
-    },
-    {
-      id: 4,
-      source: 'A',
-      target: 'D',
-      amount: 100,
-      description: 'groceries'
-    },
-    {
-      id: 5,
-      source: 'A',
-      target: 'C',
-      amount: 250,
-      description: 'other'
-    }
-  ];
-  expect(findDuplicateTransactions(transactions)).toEqual([
-    {
-      id: 1,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
-    },
-    {
-      id: 3,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
-    }
-  ]);
+    const transactions: Transaction[] = [
+        {
+            id: 1,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 2,
+            source: 'B',
+            target: 'C',
+            amount: 1000,
+            description: 'rent'
+        },
+        {
+            id: 3,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 4,
+            source: 'A',
+            target: 'D',
+            amount: 100,
+            description: 'groceries'
+        },
+        {
+            id: 5,
+            source: 'A',
+            target: 'C',
+            amount: 250,
+            description: 'other'
+        }
+    ];
+    expect(findDuplicateTransactions(transactions)).toEqual([
+        {
+            id: 1,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 3,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        }
+    ]);
+});
+
+test('No duplicate transactions', () => {
+    const transactions: Transaction[] = [
+        {
+            id: 1,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 2,
+            source: 'B',
+            target: 'C',
+            amount: 1000,
+            description: 'rent'
+        },
+        {
+            id: 3,
+            source: 'A',
+            target: 'B',
+            amount: 3000,
+            description: 'tikkie'
+        },
+        {
+            id: 4,
+            source: 'A',
+            target: 'D',
+            amount: 100,
+            description: 'groceries'
+        },
+        {
+            id: 5,
+            source: 'A',
+            target: 'C',
+            amount: 250,
+            description: 'other'
+        }
+    ];
+    expect(findDuplicateTransactions(transactions)).toEqual([]);
+});
+
+test('Multiple duplicate transactions', () => {
+    const transactions: Transaction[] = [
+        {
+            id: 1,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 2,
+            source: 'B',
+            target: 'C',
+            amount: 1000,
+            description: 'rent'
+        },
+        {
+            id: 3,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 4,
+            source: 'A',
+            target: 'D',
+            amount: 100,
+            description: 'groceries'
+        },
+        {
+            id: 5,
+            source: 'B',
+            target: 'C',
+            amount: 1000,
+            description: 'rent'
+        }
+    ];
+    expect(findDuplicateTransactions(transactions)).toEqual([
+        {
+            id: 1,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 3,
+            source: 'A',
+            target: 'B',
+            amount: 300,
+            description: 'tikkie'
+        },
+        {
+            id: 2,
+            source: 'B',
+            target: 'C',
+            amount: 1000,
+            description: 'rent'
+        },
+        {
+            id: 5,
+            source: 'B',
+            target: 'C',
+            amount: 1000,
+            description: 'rent'
+        }
+    ]);
 });
